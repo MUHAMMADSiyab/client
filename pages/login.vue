40812da422f0684fe08381951a518b36ce2b817b
MUHAMMAD Siyab <siyab.dev@gmail.com>
2022-04-12 15:14:38 +0500

    initial commit

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click.prevent="login">Login</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth.loginWith('laravelSanctum', {
+        data: {
+          email: 'john@gmail.com',
+          password: 'johnadmin',
+        },
+      })
+    },
+  },
+}
+</script>
---
+++ pages/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome {{ $auth.user.name }}</h1>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$axios
+      .get('api/test', { withCredentials: true })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
+  },
+}
+</script>
